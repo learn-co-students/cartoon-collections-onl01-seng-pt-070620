e72d7fdc97d176a44f7a10675ae73af8b8fb8e8d
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-14 03:25:04 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarve_names)
+  dwarve_names.each_with_index do|item, index| 
+  puts "#{index+1} #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|name| name.capitalize.insert(-1,"!")}
 end
 
 def long_planeteer_calls# code an argument here
