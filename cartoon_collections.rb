d28a2f849b098e694f23d46e44fdffc67adb215d
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-17 01:14:19 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+binding.pry
+def summon_captain_planet(planeteer_calls)
+  collection = []
+  planteer_calls.join("!")
+  collection << planteer_calls.upcase
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(characters)
+characters.any? do |character|
+  character.length.any? > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find{|cheddar| "cheddar"}
 end
